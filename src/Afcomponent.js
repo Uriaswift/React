c21f4b1141cf3eb07a61ef9e1a3b4d26b9075779
Andrey <Uriaswift@gmail.com>
2021-05-17 17:08:23 +0500

    init commit

---
+++ src/Afcomponent.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const Afcomponent = () => {
+    return(
+        <div>
+            <h1>Arrow function component</h1>
+        </div>
+    )
+}
+export default Afcomponent;
--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import Fcomponent from "./Fcomponent";
+import Ccomponent from "./Ccomponent";
+import Afcomponent from "./Afcomponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          {/*<Fcomponent name="Andrey"/>*/}
+          <Ccomponent/>
+          {/*<Afcomponent/>*/}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Очень жарко!
         </p>
-        <a
+        {/*<a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>*/}
       </header>
     </div>
   );
